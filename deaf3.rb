f1e3c86560ed8beb0575862a99260d83fa571dbc
John Armstrong <joarmstro@gmail.com>
2015-05-14 21:26:49 -0400

    Moved some things into functions

--- deaf3.rb
+++ deaf3.rb
@@ -1,21 +1,24 @@
 puts "What did you say?"
 hesez = gets.chomp
+
+def isupcase thebits
+	if thebits == thebits.upcase
+		year = rand(50)+1930
+		puts "NO, NOT SINCE #{year}"
+	else
+		puts "HUH? SPEAK UP!"
+	end
+end
+
 byecounter = 1
-until byecounter == 3
+
+while byecounter < 3
 	if hesez=="BYE"
 		byecounter +=1
 	else
 		byecounter = 1
 	end
-
-	if hesez != hesez.upcase
-		puts "HUH? SPEAK UP!"
-	else
-		year = rand(50)+1930
-		puts "NO, NOT SINCE #{year}"
-	end
-
+	isupcase hesez
 	hesez=gets.chomp
-
 end
 
